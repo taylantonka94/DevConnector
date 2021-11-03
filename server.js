55da6b8553c889ac77a25b56da34d0d9867c92cd
Taylan Mert Tonka <taylantonka@gmail.com>
2021-11-02 23:56:18 -0400

    guncelleme

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express")
-
+const connectDB = require("./config/db")
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get("/", (req,res) => {
     res.send("API Running")
 })
 
+// Define Routes
+app.use("/api/users", require("./routes/api/user"))
+app.use("/api/auth", require("./routes/api/auth"))
+app.use("/api/post", require("./routes/api/post"))
+app.use("/api/profile", require("./routes/api/profile"))
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port${PORT}`))
