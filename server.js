c479f10a845c3fc304d69d0afc27d739e003ff68
Taylan Mert Tonka <taylantonka@gmail.com>
2021-11-03 02:50:12 -0400

    Sa

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const connectDB = require("./config/db")
 
+
+
 const app = express();
 
 // Connect Database
 connectDB();
+
+// Init MidlleWare
+app.use(express.json({extended:false}))
 
 app.get("/", (req,res) => {
     res.send("API Running")
