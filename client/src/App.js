fab533ad4f0173b8624eb980e38daf17ddecb412
Taylan Mert Tonka <taylantonka@gmail.com>
2021-11-06 02:37:26 -0400

    Front end has started

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, {Fragment} from "react"
+import './App.css'
+import {BrowserRouter as Router, Route,Routes} from "react-router-dom"
+
+
+import Navbar from "./components/layout/Navbar"
+import Landing from "./components/layout/Landing"
+import Login from "./components/auth/Login"
+import Register from "./components/auth/Register"
+
+const App = () => {
+  return (
+    <Router>
+    <Fragment>
+      <Navbar/>
+  
+      <Routes>
+      <Route exact path="/" element = {<Landing/>}/>
+
+      </Routes>
+      <section className ="container">
+        <Routes>
+          <Route exact path = "/register" element = {<Register/>}/>
+          <Route exact path = "/login" element = {<Login/>}/>
+          </Routes>
+          </section>
+
+    </Fragment>
+    </Router>
+  );
+}
+
+export default App;
