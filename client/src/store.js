5722f88d8625f3dbbabebccf5d583a12261d31d3
Taylan Mert Tonka <taylantonka@gmail.com>
2021-11-27 23:41:32 -0500

    Completed

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,4 @@
-import {createStore,applyMiddleware} from "redux";
+import {createStore,applyMiddleware,compose} from "redux";
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
@@ -7,6 +7,24 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer,initialState,composeWithDevTools(applyMiddleware(...middleware)));
+let store;
+ 
+if (process.env.NODE_ENV === "production") {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middleware))
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+}
 
 export default store;
